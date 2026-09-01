81aa88929077fc1b6a7b4312dded89dab1713d1c
shubhampandit1836-glitch <shubhampandit1836@gmail.com>
2026-09-01 13:59:30 +0530

    Update start.sh

--- deploy/start.sh
+++ deploy/start.sh
@@ -2,10 +2,11 @@
 set -e
 
 PG_BIN=/usr/lib/postgresql/17/bin
+PG_LOG=/var/lib/postgresql/data/postgres.log
 
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
     su postgres -c "$PG_BIN/initdb -D $PGDATA"
-    su postgres -c "$PG_BIN/pg_ctl -D $PGDATA -l /var/log/postgres.log start"
+    su postgres -c "$PG_BIN/pg_ctl -D $PGDATA -l $PG_LOG start"
     sleep 3
     su postgres -c "$PG_BIN/psql --command \"CREATE USER app WITH SUPERUSER PASSWORD 'apppassword';\""
     su postgres -c "$PG_BIN/createdb -O app agentic_calendar_db"
@@ -15,7 +16,7 @@ fi
 
 export DATABASE_URL="postgresql://app:apppassword@localhost:5432/agentic_calendar_db"
 
-su postgres -c "$PG_BIN/pg_ctl -D $PGDATA -l /var/log/postgres.log start"
+su postgres -c "$PG_BIN/pg_ctl -D $PGDATA -l $PG_LOG start"
 sleep 3
 cd /app/backend && DATABASE_URL=$DATABASE_URL python scripts/migrate.py
 su postgres -c "$PG_BIN/pg_ctl -D $PGDATA stop"
